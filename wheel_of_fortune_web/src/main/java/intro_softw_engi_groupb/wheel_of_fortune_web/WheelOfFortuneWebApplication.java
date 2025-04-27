b6322d1f3ca106642e97c5fd4372af50ec183ccb
Trần Nhật Minh <minh.tn225511@sis.hust.edu.vn>
2025-04-27 17:45:29 +0700

    Add some example pages

--- wheel_of_fortune_web/src/main/java/intro_softw_engi_groupb/wheel_of_fortune_web/WheelOfFortuneWebApplication.java
+++ wheel_of_fortune_web/src/main/java/intro_softw_engi_groupb/wheel_of_fortune_web/WheelOfFortuneWebApplication.java
@@ -4,18 +4,20 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import com.vaadin.flow.theme.Theme;
+import com.vaadin.flow.component.page.AppShellConfigurator;
 
 @SpringBootApplication
 @RestController
-public class WheelOfFortuneWebApplication {
+@Theme("my-theme")
+public class WheelOfFortuneWebApplication implements AppShellConfigurator {
 
-	@GetMapping("/hello")
+    @GetMapping("/hello")
     public String check() {
         return "こんにちは、世界！🇯🇵";
     }
 
-	public static void main(String[] args) {
-		SpringApplication.run(WheelOfFortuneWebApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(WheelOfFortuneWebApplication.class, args);
+    }
 }
