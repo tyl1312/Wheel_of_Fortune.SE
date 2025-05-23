cb0607c8b0139fa5feb2884412dc3d9ee2acf91c
Tyl1312 <tung13122004@gmail.com>
2025-05-23 22:39:42 +0700

    fix toDto

--- src/main/java/com/example/demo/model/User.java
+++ src/main/java/com/example/demo/model/User.java
@@ -1,39 +1,39 @@
 package com.example.demo.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.*;
-
 
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
 @Builder
 @Entity
-@Table(name = "customers")
+@Table(name = "users")
 public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "customer_id")
-    private Long id;
+    @Column(name = "user_id")
+    private int user_id;
+    private String phone_number;
+    private String full_name;
+    private String gender;
+    private Long total_spent;
+    private int spin;
 
-    @Column(name = "full_name")
-    private String name;
-
-    @Column(name = "email")
-    private String email;
-
-    @Column(name = "phone_number")
-    private String phone;
-
-    @Column(name = "password")
+    @JsonIgnore
     private String password;
-
-    @Column(name = "points")
-    private int points;
 
     @Override
     public String toString() {
-        return getClass().getSimpleName() + " (id=" + id + ", name=" + name + ", email=" + email + ", phone=" + phone + ")";
+        return getClass().getSimpleName() +
+                " (user_id=" + user_id +
+                ", full name=" + full_name +
+                ", phone number=" + phone_number +
+                ", gender=" + gender +
+                ", total spent=" + total_spent +
+                ", spin=" + spin +
+                ")";
     }
 }
