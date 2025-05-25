f64c31555c1df4e8b37d062517674dd03ca70a66
Tyl1312 <tung13122004@gmail.com>
2025-05-25 16:35:29 +0700

    fix

--- src/main/resources/static/js/register.js
+++ src/main/resources/static/js/register.js
@@ -1,14 +1,11 @@
 function matchPassword() {
     const password = document.getElementById("inputPassword").value;
     const confirmPassword = document.getElementById("confirmPassword").value;
-    const registerBtn = document.getElementById("btnRegister");
 
     if (password !== confirmPassword) {
         document.getElementById("confirmPassword").setCustomValidity("Passwords do not match");
-        registerBtn.disabled = true;
     } else {
         document.getElementById("confirmPassword").setCustomValidity("");
-        registerBtn.disabled = false;
     }
 }
 
